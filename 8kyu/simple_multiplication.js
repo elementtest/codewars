1cd6d62336fcfc795fc9ae0e163a6b0bd61f0c4f
elementtest <elementtest7@gmail.com>
2023-01-15 16:19:43 +0800

    comment

--- 8kyu/simple_multiplication.js
+++ 8kyu/simple_multiplication.js
@@ -1,4 +1,4 @@
-This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
 function simpleMultiplication(number) {
     if (number % 2 === 0) {
