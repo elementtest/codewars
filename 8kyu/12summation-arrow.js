115ef7e964eaa8bd1a11495556b287cb2845cb1e
elementtest <elementtest7@gmail.com>
2023-01-18 14:30:22 +0800

    summation

---
+++ 8kyu/12summation-arrow.js
@@ -0,0 +1,22 @@
+//write a program that finds the summation of every number from 1 to num.
+const summation = (num) => (num * (1 + num)) / 2;
+
+//started out using a for loop that was complicated and changed it to fit with a 
+/*
+tutorial.. tutorial pointed out that you can use the math formula to make it much 
+more consice and easy to read. 
+
+What i learned?
+Try and look up the formula and it will be way easier instead of doing multiple lines
+to do a loop.  
+
+Also arrow functions:  
+
+function keyword is gone.  
+
+you need to declare a variable using let or const. 
+
+const nameOfConst = (PARAMETERS) => EXECUTE CODE;
+
+and the return is assumed in an arrow function 
+*/
