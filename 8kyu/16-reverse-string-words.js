29257513496e14f7e237746fb3a1dc1209af2e77
elementtest <elementtest7@gmail.com>
2023-01-21 01:30:05 +0800

    reverse a string of words

---
+++ 8kyu/16-reverse-string-words.js
@@ -0,0 +1,31 @@
+//take a string of words ina  sentence and reverse it
+
+ return str.split(" ").reverse().join(" ");
+
+ //something that frequently happens in web development
+ //converting between different data types i.e. between strings and arrays
+
+ //array method join 
+
+ //string method split 
+ /*
+if you wanna convert forma  string to an array you call the split method.  
+will take the entire string and split it based on what you place as the argument
+of the method... i think it has to find it in the string though. lets try that .  ok so im right... the split method has to be related to the string. i.e. if your string doesnt have a q then you cant use q to split it. 
+
+you can also use regular expressions with the split method
+
+split also turns the original string into an array k
+by using split with an empty string you will be splitting on every single character
+
+every single character there is nothing so that is what you are splitting it on 
+
+string.split('')
+
+
+
+JOIN METHOD
+join will do the exact opposite of split and just join 
+
+
+ */
