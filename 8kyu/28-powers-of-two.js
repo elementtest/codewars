afa4a997f7d0a393256281c097fdde7e25d148d3
elementtest <elementtest7@gmail.com>
2023-02-08 12:46:59 +0800

    powers of 2

---
+++ 8kyu/28-powers-of-two.js
@@ -0,0 +1,8 @@
+//takes a non negative integer n as input and returns a list of all the powers of 2 with the exponent ranging from 0 to n
+function powersOfTwo(n) {
+  const arr = [];
+  for (let i = 0; i <= n; i++) {
+    arr.push(Math.pow(2, i));
+  }
+  return arr;
+}
