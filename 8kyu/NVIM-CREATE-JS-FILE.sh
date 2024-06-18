17d01ae78426002e85513a6f459fb6783837a4ac
elementtest <elementtest8@gmail.com>
2024-06-18 21:57:57 +0800

    14484346632599

--- 8kyu/NVIM-CREATE-JS-FILE.sh
+++ 8kyu/NVIM-CREATE-JS-FILE.sh
@@ -5,5 +5,6 @@ read -p "file name ? " filename
 touch "$filename"
 read -p "instructions of the challenge? " shuoming
 echo "$shuoming" >> "$filename"
+# use xclip to take copy / paste register from system clipboard and append it to file
 xclip -selection clipboard -o >> "$filename"
 cat $filename
