adeb351d7f3fbea7fb64d7fd833a16f2aaa876f5
elementtest <elementtest8@gmail.com>
2024-06-18 21:56:03 +0800

    CREATE NEW SCRIPT TO DO COPY PASTE CODE WARS PROJECTS!!!!! 224882376425933

--- 8kyu/create-js-file-script.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-#script to make files and add codewars solutions to be uploaded to github
-
-read -p "file name ? " filename
-touch "$filename"
-read -p "instructions of the challenge? " shuoming
-echo "$shuoming" >> "$filename"
-code "$filename"
