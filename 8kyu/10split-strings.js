5dcd7c24eacd6e4f7c32c26d3eb4b851c0e658f4
elementtest <elementtest8@gmail.com>
2024-06-27 19:32:13 +0800

    2665251038527

--- 8kyu/10split-strings.js
+++ 8kyu/10split-strings.js
@@ -1,8 +1,13 @@
 //reverse a string
-function solution(str) {
-  var x = str.split("");
-  x = x.reverse("");
-  x = x.join("");
-  return x;
-}
+//function solution(str) {
+//  var x = str.split("");
+//  x = x.reverse("");
+//  x = x.join("");
+//  return x;
+//}
 //reverse a string
+function solution(str) {
+    return str.split("").reverse().join("");
+}
+//arrow function 
+const solution = str => str.split('').reverse().join("");
