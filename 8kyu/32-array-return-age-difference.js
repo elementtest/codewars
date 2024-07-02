657bbe73926fad7fa327e8467a19fe1b564f7871
elementtest <elementtest8@gmail.com>
2024-07-03 01:48:06 +0800

    157022344528171

--- 8kyu/32-array-return-age-difference.js
+++ 8kyu/32-array-return-age-difference.js
@@ -1,16 +1,4 @@
 //
--- CSS KEYMAP to expand star asterisk
-    vim.keymap.set('i', '*<leader>', function()
-        -- Insert the characters * { }
-        vim.api.nvim_put({ '* { }' }, 'c', true, true)
-
-        -- Move the cursor back inside the braces
-        local cursor = vim.api.nvim_win_get_cursor(0)
-        vim.api.nvim_win_set_cursor(0, { cursor[1], cursor[2] - 2 })
-
-        -- Simulate pressing the Enter key
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, false, true), 'n', true)
-    end, { noremap = true, silent = true })
 //Find the Difference in Age between Oldest and Youngest Family Members
 //return an array with max age, min age and age diff
 function differenceInAges(ages){
