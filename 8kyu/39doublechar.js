73f4a3ee611c15596b6d7d34f582a9c86e614c58
elementtest <elementtest8@gmail.com>
2024-07-15 20:00:10 +0800

    39th code war exercise completed 13103797515621

---
+++ 8kyu/39doublechar.js
@@ -0,0 +1,16 @@
+// take a string and then double the characters in in it and return the new string
+// you had an issue where you were declaring the variable but not initializing it.  
+// this basically made it have undefined at the beginning of the string 
+function doubleChar() {
+    let str = 'abcd';
+    str = str.split('');
+    let strTwo = '';
+    for (let i = 0; i < str.length; i++) {
+        strTwo += str[i];
+        strTwo += str[i];
+    }
+    console.log(strTwo);
+
+
+}
+doubleChar();
