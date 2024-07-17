b614e6144dcad90ce513c3d7a62faadebf8c05e7
elementtest <elementtest8@gmail.com>
2024-07-17 22:42:16 +0800

    nvim surround practice 304252797221844

--- 8kyu/13EXTRA-paperwork.js
+++ 8kyu/13EXTRA-paperwork.js
@@ -1,7 +1,9 @@
 function paperwork() {
-    let n = Math.floor(Math.random()*100) + 1;
-    let m = Math.floor(Math.random()*100) + 1;
+    let sign = Math.random() > 0.5 ? -1 : 1;
+    let n = ( ( Math.floor(Math.random()*100) + 1) * sign ) ;
+    let m = ( Math.floor(Math.random()*100) + 1) * sign ;
     console.log(`n is ${n} and m is ${m}`);
     return (n < 0 || m < 0) ? 0 : (n * m);
 }
 console.log(paperwork());
+this is a sentence
