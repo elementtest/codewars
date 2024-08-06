d517ffe83b2bf20349b065a14a7631f54efcf662
elementtest <elementtest8@gmail.com>
2024-08-06 22:26:19 +0800

    194082168614439

---
+++ 7kyu/object-practice.js
@@ -0,0 +1,8 @@
+function practice() {
+    const obb = {"Java": 10, "Ruby": 80, "Python": 65};
+    console.log(Object.values(obb));
+    //console.log(Object.keys(obb));
+    //console.log(Object.entries(obb));
+
+}
+practice();
