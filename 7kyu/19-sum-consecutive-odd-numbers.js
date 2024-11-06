b4c34b5e9d77d07be555420c6f516c31b2bbdee1
elementtest <elementtest8@gmail.com>
2024-11-06 22:30:00 +0800

    16664794016695

---
+++ 7kyu/19-sum-consecutive-odd-numbers.js
@@ -0,0 +1,11 @@
+let n = 2;
+function rowSumOddNumbers(n) {
+    for (let i = 1; i <=n; i++) {
+        if (n>1) {
+            n + (n+2);
+        }
+    }
+    return n;
+}
+
+console.log(rowSumOddNumbers(n));
