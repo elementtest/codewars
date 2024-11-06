a623f626087f54fd915d249f09ae26989f73c606
elementtest <elementtest8@gmail.com>
2024-11-06 22:29:21 +0800

    9318545112578

--- 6kyu/1-3-or-5-multiple.js
+++ 6kyu/1-3-or-5-multiple.js
@@ -1,6 +1,18 @@
-let number = 6;
+let number = 93;
 function solution(number) {
-    return number * -1 >= 0 ? 0 : 
-
+    let arr = [];
+    for (i=3; i<number; i+=3) {
+        arr.push(i)
+    }
+    for (i=5; i<number; i+=5) {
+        arr.push(i)
+    }
+    console.log(arr.reduce((acc, num) => acc + num, 0));
 
 }
+solution(number);
+
+/*
+error... i was using i + 3... that does not update i resulting in infinite loop
+have to use i+=3... then i will be updated
+*/
