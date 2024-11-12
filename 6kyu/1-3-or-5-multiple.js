7270c8e52de568da0ad11a32788828b00c34007e
elementtest <elementtest8@gmail.com>
2024-11-12 22:06:59 +0800

    115632566012706 FIRST 6 KYU COMPLETED!!!!!!!!!!!!!

--- 6kyu/1-3-or-5-multiple.js
+++ 6kyu/1-3-or-5-multiple.js
@@ -1,5 +1,8 @@
-let number = 93;
+let number = 60;
 function solution(number) {
+    if (Math.sign(number) === -1) {
+        return 0;
+    } else {
     let arr = [];
     for (i=3; i<number; i+=3) {
         arr.push(i)
@@ -7,12 +10,24 @@ function solution(number) {
     for (i=5; i<number; i+=5) {
         arr.push(i)
     }
-    console.log(arr.reduce((acc, num) => acc + num, 0));
-
+    let uniArr = [...new Set(arr)];
+    console.log(uniArr);
+    console.log(uniArr.reduce((acc, num) => acc + num, 0));
+    }
 }
-solution(number);
+console.log(solution(number))
 
 /*
 error... i was using i + 3... that does not update i resulting in infinite loop
 have to use i+=3... then i will be updated
+*/
+
+/*
+lets start at the beginning .. the Math.sign method helps return if a number is positive negave or 0
+
+Math.sign(number) posssible outcomes:::: 
+1-> number is positive
+-1 > number is negative
+0 > the number is 0
+
 */
