9953c8425fffa5ecf8a8beaa6cec6043168a03ce
elementtest <elementtest8@gmail.com>
2024-11-14 19:53:43 +0800

    16389406514736

--- 6kyu/2-reverse-letters.js
+++ 6kyu/2-reverse-letters.js
@@ -5,16 +5,17 @@
 2. only consist of letters and spaces
 3.  spaces included only when more than one word is present
 */
-/*
-one of the most frequently asked javascript questions in interviews
-    */
-let string = "the quick dog";
+let string = "Just kidding there is still one more";
 function spinWords(string) {
     let spp = string.split(" ");
     for (let i = 0; i<spp.length; i++) {
+        if (spp[i].length >= 5) {
+            spp[i] = spp[i].split("").reverse().join("");
+        }
+        }
+    return spp.join(" ");
 
-    }
-}
+        }
 console.log(spinWords(string));
 
 /*
