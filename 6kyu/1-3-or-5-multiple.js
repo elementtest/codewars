e8691e715adedb756619f0c9b3c5e7efed5617ad
elementtest <elementtest8@gmail.com>
2024-11-14 19:23:44 +0800

    3093121522687

--- 6kyu/1-3-or-5-multiple.js
+++ 6kyu/1-3-or-5-multiple.js
@@ -31,3 +31,17 @@ Math.sign(number) posssible outcomes::::
 0 > the number is 0
 
 */
+
+/*
+GOD SOLUTION: 
+function solution(number) {
+var sum = 0;
+
+for (var i = 1; i< number; i++) {
+if (i % 3 == 0 || i % 5 == 0) {
+sum+= i
+}
+}
+return sum;
+}
+*/
---
+++ 6kyu/2-reverse-letters.js
@@ -0,0 +1,22 @@
+/*
+1. take in a string
+    if 5 or more letters
+    then reverse
+2. only consist of letters and spaces
+3.  spaces included only when more than one word is present
+*/
+/*
+one of the most frequently asked javascript questions in interviews
+    */
+let string = "the quick dog";
+function spinWords(string) {
+    let spp = string.split(" ");
+    for (let i = 0; i<spp.length; i++) {
+
+    }
+}
+console.log(spinWords(string));
+
+/*
+
+*/
