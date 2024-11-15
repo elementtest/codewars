5b49d30fbc4d642755dae628f9ef29962c5b7a07
elementtest <elementtest8@gmail.com>
2024-11-15 16:42:56 +0800

    25039110975617

---
+++ 6kyu/function-review.js
@@ -0,0 +1,7 @@
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+greet("Allice");
+/*
+can directly put the argument in the function call >>> greet("Alice") Alice being the argument
+*/
