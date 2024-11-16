ef179b30c85c46a5571c3ffd8f11e13970a9fa36
elementtest <elementtest8@gmail.com>
2024-11-16 13:13:29 +0800

    262152958428412

--- 6kyu/3-facebook-likes.js
+++ 6kyu/3-facebook-likes.js
@@ -2,4 +2,21 @@
 create a string similar to FB's so-and-so likes this post
 */
 
+function likes(names) {
+    let arrl = names.length
+    switch (arrl) {
+        case 0: 
+            return "no one likes this";
+        case 1:
+            return `${names[0]} likes this`;
+        case 2: 
+            return `${names[0]} and ${names[1]} like this`;
+        case 3: 
+            return `${names[0]}, ${names[1]}, and ${names[2]} like this`;
+        default: 
+            return `${names[0]}, ${names[1]} and ${arrl -2} others like this`;
+    }
+}
+
+console.log(likes(['Bill', 'Mark', 'Stew']))
 
