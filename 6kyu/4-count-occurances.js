5f227fd4fb8eca0715fc1031899df1423a61d959
elementtest <elementtest8@gmail.com>
2024-11-20 22:39:21 +0800

    211210136676

--- 6kyu/4-count-occurances.js
+++ 6kyu/4-count-occurances.js
@@ -6,14 +6,19 @@ find the number (Only one) that occurs an odd amount of times
 function findOdd(A) {
     let minn = Math.min(...A);
     let maxx = Math.max(...A);
-    for (let i = 0; i < A.length; i++) {
-        for (let j = minn; j <= maxx; j++) {
-            if (A[i] === j) {
-                let countt = 0;
-                countt++;
-                console.log(countt);
+    for (let i = 0; i <= A[i]; i++) {
+        for (let j = minn; j<= maxx; j++) {
+           let filtered = A.filter(boo => boo === j)
+            if (!(filtered.length % 2 === 0)) {
+                return j;
             }
         }
     }
+
 }
 findOdd([0,1,1,1,2,3,4]);
+console.log(findOdd([0,0,1,1,1,2,2,3,3,4,4]));
+/*
+GOD SOLUTION 
+const findOdd = (xs) => xs.reduce((a,b) => a ^ b);
+*/
