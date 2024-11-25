c305a70caaeed9cbdae22d0a23f48da4939cb6bf
elementtest <elementtest8@gmail.com>
2024-11-25 19:36:06 +0800

    41313250622309

--- 6kyu/5-difference-array.js
+++ 6kyu/5-difference-array.js
@@ -3,3 +3,16 @@ given two arrays of numbers
 find commonalities of both
 remove all occurances of commonalities
 */
+let nOne = [1,1,3,4,5];
+let nTwo = [6,3,8,3,1,1];
+function killComm(nOne, nTwo) {
+    for (let i = 0; i <= nOne.length; i++) {
+        console.log(nOne[i]);
+        for (let j = 0; j <= nTwo.length; j++) {
+            console.log(nTwo[j]);
+        }
+    }
+}
+
+console.log(killComm(nOne, nTwo));
+
