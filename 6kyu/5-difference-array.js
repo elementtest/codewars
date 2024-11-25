96dbeeb5dfd883c9c6388ebe6cc8fda22d905794
elementtest <elementtest8@gmail.com>
2024-11-25 19:52:12 +0800

    26122465926804

--- 6kyu/5-difference-array.js
+++ 6kyu/5-difference-array.js
@@ -6,13 +6,13 @@ remove all occurances of commonalities
 let nOne = [1,1,3,4,5];
 let nTwo = [6,3,8,3,1,1];
 function killComm(nOne, nTwo) {
-    for (let i = 0; i <= nOne.length; i++) {
-        console.log(nOne[i]);
-        for (let j = 0; j <= nTwo.length; j++) {
-            console.log(nTwo[j]);
+    for (let i = 0; i < nOne.length; i++) {
+        for (let j = 0; j < nTwo.length; j++) {
+            let comm = nOne.filter(boo => boo === nTwo[j]);
+        }
         }
     }
-}
+
 
 console.log(killComm(nOne, nTwo));
 
