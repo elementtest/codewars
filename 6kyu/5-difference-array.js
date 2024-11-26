abf14139ad7de326d181cbce03f3453f76117fd7
elementtest <elementtest8@gmail.com>
2024-11-26 22:19:00 +0800

    29973889731922

--- 6kyu/5-difference-array.js
+++ 6kyu/5-difference-array.js
@@ -6,13 +6,16 @@ remove all occurances of commonalities
 let nOne = [1,1,3,4,5];
 let nTwo = [6,3,8,3,1,1];
 function killComm(nOne, nTwo) {
-    for (let i = 0; i < nOne.length; i++) {
-        for (let j = 0; j < nTwo.length; j++) {
-            let comm = nOne.filter(boo => boo === nTwo[j]);
-        }
-        }
-    }
+    let emptArray = [];
+    emptArray = [...nOne, ...nTwo];
+    return emptArray;
+}
 
 
 console.log(killComm(nOne, nTwo));
+/*
+notes was going the route of doing two four loops but i dont think i need to do that... all i really 
+need to do is put all the values into one array and check to see if there are duplicates.  
+i think this is the right way of thinking about it
+    */
 
