ba3a8b646f2b6c6744d6583785ba2d2650ad7d5e
elementtest <elementtest8@gmail.com>
2024-11-29 22:10:47 +0800

    205221673428941

--- 6kyu/6-digit-sum.js
+++ 6kyu/6-digit-sum.js
@@ -16,11 +16,11 @@ let n = 1234;
 function digitalRoot(n) {
     n = n.toString()
     n = n.split('');
+    n = n.map(num => parseInt(num));
     let countt = 0;
     if (n.length >= 2) {
         for (let i = 0; i < n.length; i++) {
             n[i] += countt;
-            return countt;
         
         }
         return countt;
