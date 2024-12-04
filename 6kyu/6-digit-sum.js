4b8724c7cedcb30b555cacda2d9203eddb9bcc06
elementtest <elementtest8@gmail.com>
2024-12-04 22:07:59 +0800

    241532283125556

--- 6kyu/6-digit-sum.js
+++ 6kyu/6-digit-sum.js
@@ -12,22 +12,24 @@ Examples
 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
 
 */
-let n = 493193;
+let n = 16;
 function digitalRoot(n) {
-    n = n.toString();
-    n = Array.from(n, Number);
-    let countt = 0;
-
-    for (let i = 0; i < n.length; i++) {
-        countt += n[i];
-    }  
-
-        
-    return countt;
+    while (n >= 10) {
+    n = n.toString().split('').reduce((accu, numm) => accu + Number(numm), 0);
+    }
+    return n;
 }
 
 
 console.log(digitalRoot(n));
 /* 
-do not put the return statement in the for loop because on the first round it will immediately exit
+SOLUTION:  
+function digital_root(n) {
+return (n -1) % 9 + 1;
+}
+
+notes::
+i was missing while.... repeatedly execute a block of code as long as a specified condition is true.  
+n equals n to string no quotes... puts it in a string... split with quotes breaks up each individual space and puts it in an array for reduce.  reduce params accumulator and the number itself arrow function (not done yet) Number(numm), 0 start from zero...... will continue looping until n is less than or not equal to ten.  put the return n OUTSIDE of the while loop... 
+
 */
