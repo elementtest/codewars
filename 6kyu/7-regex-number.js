081c38b28c95c24dee2bc5f91bf8befd18b16eb0
elementtest <elementtest8@gmail.com>
2024-12-06 22:04:25 +0800

    170851380613743

---
+++ 6kyu/7-regex-number.js
@@ -0,0 +1,24 @@
+/*
+intake of an array and output a valid phone number string
+
+possible regex that can help: 
+{n} mathes n occurances of the preceding element.  
+
+\d matches any digit 0 to 9
+
+\(\d{3}\) \d{3}-\d{4}
+*/
+let numbers = ([1,2,3,4,5,6,7,8,9,0]);
+function createPhoneNumber(numbers) {
+
+}
+
+
+/*
+more notes... i guess... we just take some notes and then think about it and come back to it later
+
+let regexx = /\(\d{3}\) \d{4}/;
+
+
+
+*/
