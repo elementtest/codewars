895e57ffc798987805823ac57863847271908313
elementtest <elementtest8@gmail.com>
2024-12-07 13:09:34 +0800

    15980841216571

---
+++ 6kyu/7-regex-telephone.js
@@ -0,0 +1,13 @@
+/*
+write a function that accepts an array of 10 integers (between 0 and 9) that returns a string 
+of those numbers in the form of a phone number
+*/
+
+let numbers = [1,2,3,4,5,6,7,8,9,0];
+
+function createPhoneNumber(numbers) {
+    numbers = numbers.join('').toString();
+    let regexx = 
+}
+
+console.log(createPhoneNumber(numbers));
