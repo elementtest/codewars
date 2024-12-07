89f685388d61893576b02a3674cf9f7220e233c0
elementtest <elementtest8@gmail.com>
2024-12-07 13:02:56 +0800

    12731394324443

---
+++ 6kyu/7-test-reg.js
@@ -0,0 +1,46 @@
+/*
+intake of an array and output a valid phone number string
+
+possible regex that can help: 
+{n} mathes n occurances of the preceding element.  
+
+\d matches any digit 0 to 9
+
+\(\d{3}\) \d{3}-\d{4}
+*/
+let numbers = ([1,2,3,4,5,6,7,8,9,0]);
+function createPhoneNumber(numbers) {
+
+}
+
+
+/*
+more notes... i guess... we just take some notes and then think about it and come back to it later
+
+let regexx = /\(\d{3}\) \d{4}/;
+
+var regex = /hello/;
+
+var str = 'hello world';
+
+var result = regex.test(str);
+
+console.log(result);
+
+returns true;
+
+tests whetehr or not a match has been found... it accepts a string which we have to test against a regular expression and returns true
+
+var regex equals the telephone number
+
+regex.test('input string that you got from the array')
+
+var regexx = telephone regexx
+
+regexx.test('input string')
+
+if equals true then return it in string format
+
+
+
+*/
