20bf41b73c4f5a8dfd01b0d792d98063e07f584c
elementtest <elementtest8@gmail.com>
2024-12-26 22:44:52 +0800

    31953304625256

--- 6kyu/10-count-number-of-repeating-letters.js
+++ 6kyu/10-count-number-of-repeating-letters.js
@@ -1,9 +1,30 @@
 /*
 count the number of repeating letters in a string
 */
-let str = 'aabbcdefgg';
+let str = 'aabbcde';
 function countRepeat(str) {
-    for (let i = 0; i < str.length; i++) {
-        
-    }
-}
+    str = str.toLowerCase().split('').sort();
+    console.log(str);
+    let initt = str[0];
+    let countt = 0;
+    let timm = 0;
+    for (let i = 1; i < str.length; i++) {
+        if (initt === str[i]) {
+            if (timm === 0) {
+                countt++;
+                timm++;
+            }
+            
+        } else {
+            initt = str[i];
+            if (timm === 1) {
+                timm--;
+            }
+        }
+        }
+    return countt;
+    
+        }
+
+
+console.log(countRepeat(str));
