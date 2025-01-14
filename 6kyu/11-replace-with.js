649597107e2945ea0c703e07bf4d28a5a1dd968e
elementtest <elementtest8@gmail.com>
2025-01-14 22:07:50 +0800

    includes complete first draft 11404506518030

--- 6kyu/11-replace-with.js
+++ 6kyu/11-replace-with.js
@@ -1,13 +1,21 @@
-let str = "bookkeeper";
+let str = "(( @";
+
+str = str.toLowerCase().split('');
+
+let same = [];
 
 for (let i = 0; i < str.length; i++) {
     for (let j = i + 1; j < str.length; j++) {
         if (str[i] === str[j]) {
-            console.log(str[j]);
-
+            same.push(str[i]);
         }
     }
 }
+same = [...new Set(same)];
+
+const newArr = str.map(letter => same.includes(letter) ? ')' : '(');
 
 
 
+
+console.log(newArr.join(''));
