9c2d23c793d64ed2cf155c86a889bb89166c4de8
elementtest <elementtest8@gmail.com>
2025-02-03 19:42:23 +0800

    103402015430369

---
+++ 6kyu/14-persistent-bugger.js
@@ -0,0 +1,9 @@
+/*
+write a function persistence, that takes in a positive parameter num and returns its multiplicative persistence which is the number of times you must multiply the digits in num until you reach a single digit
+
+example input output
+39 ==> 3 because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 only has one digit there are 3 multiplications
+999 ==> 4 because 9*9*9 = 729, 7*2*9 = 126, 1 * 2 * 6 = 12, and finally 1* 2 = 2
+/*
+
+
