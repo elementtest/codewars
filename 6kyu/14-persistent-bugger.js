a76aabdbaf75dc69a73da8a024d4f307c579a131
elementtest <elementtest8@gmail.com>
2025-02-03 20:08:31 +0800

    while practice 25283323164801

--- 6kyu/14-persistent-bugger.js
+++ 6kyu/14-persistent-bugger.js
@@ -4,6 +4,7 @@ write a function persistence, that takes in a positive parameter num and returns
 example input output
 39 ==> 3 because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 only has one digit there are 3 multiplications
 999 ==> 4 because 9*9*9 = 729, 7*2*9 = 126, 1 * 2 * 6 = 12, and finally 1* 2 = 2
-/*
 
+take the number and split it into an array... take array and multiply numbers together... if numbers are greater than 10 then repeat
+*/
 
