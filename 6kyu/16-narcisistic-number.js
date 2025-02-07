a3a856db98f46145f1367063feaef709006e80d3
elementtest <elementtest8@gmail.com>
2025-02-07 22:46:15 +0800

    narc function complete! 168321702430300

--- 6kyu/16-narcisistic-number.js
+++ 6kyu/16-narcisistic-number.js
@@ -2,3 +2,14 @@
 narcissistic number is a number that is split up and then raised to the power of the number of digits 
 given input of a number output whether true or false regarding that number being a narcisistic number or not
 */
+
+function narcissistic(value) {
+
+let newVal = value.toString().split('').map(num => {return Number(num) ** (value.toString().length)}).reduce((accu, finnum) => {
+    return accu + finnum;
+}, 0) 
+    return newVal === value ? true : false;
+
+}
+
+console.log(narcissistic(153));
