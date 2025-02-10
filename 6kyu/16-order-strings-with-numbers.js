a3e03939c85a956a078731e08bddd2a09960279c
elementtest <elementtest8@gmail.com>
2025-02-10 20:07:12 +0800

    working on problem 23694696829017

--- 6kyu/16-order-strings-with-numbers.js
+++ 6kyu/16-order-strings-with-numbers.js
@@ -7,15 +7,3 @@ becomes
 this is a test( with the numbers included in the string)
 */
 
-function order(words) {
-    const regX = /\d+/gm;
-    const result = words.match(regX);
-    for (i = 0; i < words.length; i++) {
-        console.log('ok');
-        console.log(words[i].match(regX))
-
-    }
-
-}
-
-console.log(order("Thi1s is2 3a T4est"));
