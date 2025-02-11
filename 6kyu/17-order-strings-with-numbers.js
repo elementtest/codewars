5b28a82a09767f7fe7935fcc7d86721fd02783c3
elementtest <elementtest8@gmail.com>
2025-02-11 22:19:53 +0800

    new solution 147712296129667

--- 6kyu/17-order-strings-with-numbers.js
+++ 6kyu/17-order-strings-with-numbers.js
@@ -7,3 +7,18 @@ becomes
 this is a test( with the numbers included in the string)
 */
 
+
+function order(words) {
+    const regX = /\d/;
+
+    words = words.split(' ');
+
+    let result = words.sort((a,b) => {
+        const numA = a.match(regX);
+        const numB = b.match(regX);
+
+        return numA - numB;
+    })
+    return result.join(' ');
+}
+console.log(order("is2 Thi1s T4est 3a" ));
