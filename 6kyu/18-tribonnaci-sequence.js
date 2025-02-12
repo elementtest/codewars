5819145e609ef7e62e8e5011e029dad13109fffc
elementtest <elementtest8@gmail.com>
2025-02-12 22:59:31 +0800

    tribonacci first steps 21367716123884

--- 6kyu/18-tribonnaci-sequence.js
+++ 6kyu/18-tribonnaci-sequence.js
@@ -3,7 +3,17 @@ goal is to given an array to take the past three numbers and then to add them to
 next number in the array
 i.e. 
 1,1,1,3,5
-you will also be given n = something on what your goal is to increase the array to.  
+you need to create a tribonacci function that given a signature array/list, returns the 
+first n elements - signature included of the so needed sequence
 */
 
 
+function tribonaci(signature, n) {
+    let newVall = 0;
+    for (let i = 0; i < signature.length; i++) {
+        console.log(signature[i]);
+    }
+    return newVall;
+
+}
+console.log(tribonaci([1,1,1], 10));
