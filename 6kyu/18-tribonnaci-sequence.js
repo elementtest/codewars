936fa2331362bacf7ab4a830c802f3647f6d5da8
elementtest <elementtest8@gmail.com>
2025-02-12 22:45:53 +0800

    tribonacci challenge 12866265265812

---
+++ 6kyu/18-tribonnaci-sequence.js
@@ -0,0 +1,9 @@
+/*
+goal is to given an array to take the past three numbers and then to add them together to create the 
+next number in the array
+i.e. 
+1,1,1,3,5
+you will also be given n = something on what your goal is to increase the array to.  
+*/
+
+
