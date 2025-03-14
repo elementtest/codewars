c7f3f1a1241d9329dc3744b728bca22c6792b231
elementtest <elementtest8@gmail.com>
2025-03-14 23:12:34 +0800

    name change

--- 6kyu/21-morse-code.js
+++ 6kyu/21-morse-code.js
@@ -4,7 +4,7 @@ Given an input of morse code, output the message in English it would produce.
 first time using a predefinited object dictionary named MORSE_CODE
 */
 
-const extendedMorseCodeDict = {
+const MorseDict = {
   ".-": "A",
   "-...": "B",
   "-.-.": "C",
