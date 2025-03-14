6758ebf74d0fd5899aa1d65fc1a7c2fdc36e9576
elementtest <elementtest8@gmail.com>
2025-03-14 22:21:02 +0800

    add dictionary

--- 6kyu/21-morse-code.js
+++ 6kyu/21-morse-code.js
@@ -3,3 +3,58 @@ given an input of morse code, output the message in English it would produce.
 
 first time using a predefinited object dictionary named MORSE_CODE
 */
+
+const extendedMorseCodeDict = {
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  
+  "-----": "0",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  
+  "...---...": "SOS",  // Special code for SOS
+  
+  ".-.-.-": ".",  // Period
+  "--..--": ",",  // Comma
+  "..--..": "?",  // Question mark
+  "-....-": "-",  // Dash
+  ".----.": "'",  // Apostrophe
+  "-.-.--": "!",  // Exclamation mark
+  "-..-.": "/",  // Slash
+  ".-..-.": "\"", // Double quote
+  ".--.-.": "@",  // At sign (@)
+};
+
+
+
