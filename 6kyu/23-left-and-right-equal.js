26d695ff4917743a39775b176c460972179f7376
elementtest <elementtest8@gmail.com>
2025-03-28 22:01:20 +0800

    start left equal to right array problem

---
+++ 6kyu/23-left-and-right-equal.js
@@ -0,0 +1,3 @@
+/*
+find the index position N in an array where the sum of all the integers on the right side of it.  and the sum of all the integers to the left side of it are equal to one another.  if this does not exist then return -1 
+*/
