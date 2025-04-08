46dfc48682bba6b69932f8a60ed1a538bfd6be5d
elementtest <elementtest8@gmail.com>
2025-04-08 22:00:13 +0800

    start split string kata

--- 6kyu/24-split-strings.js
+++ 6kyu/24-split-strings.js
@@ -5,3 +5,9 @@ if string.length is an odd number:
 then final pair's missing second character: 
 place an underscore _
 */
+
+function solution(str) {
+    return str.split(/.{2,}/);
+}
+
+console.log(solution('abcdefghij'));
