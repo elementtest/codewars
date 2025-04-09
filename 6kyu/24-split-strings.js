f13a1a77c98d030bfced890c1a3a1f6bf28fa9d1
elementtest <elementtest8@gmail.com>
2025-04-09 22:51:40 +0800

    notes

--- 6kyu/24-split-strings.js
+++ 6kyu/24-split-strings.js
@@ -7,7 +7,16 @@ place an underscore _
 */
 
 function solution(str) {
-    return str.split(/.{2,}/);
+    return str.split(/.{1,2}/g);
 }
 
 console.log(solution('abcdefghij'));
+
+/*
+problem... the split() method is looking at the string then 
+deleting the delimiter (fancy name to say what you are searching for to split something)
+then spliting the string based on the position of the delimiter
+so the problem you were having is you were saying str.split(/\w{2}/) which is correct
+the problem is it is deleting every two characters in the string... leaving you with nothing 
+but empty strings as a result
+*/
