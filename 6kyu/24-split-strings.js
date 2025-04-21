a4a5cf6a418d337fe6d8de64fec56c04a9c417f9
elementtest <elementtest8@gmail.com>
2025-04-21 20:42:02 +0800

    comment

--- 6kyu/24-split-strings.js
+++ 6kyu/24-split-strings.js
@@ -1,3 +1,11 @@
+/*
+given a string, break down the string into pairs of characters 
+i.e.
+abcdefghij
+'ab','cd','ef','gh','ij'
+if there is an odd amount of characters add a _ underscore at the end
+
+    */
 function solution(str) {
     if (typeof str !== 'string') return [];
 
@@ -9,3 +17,8 @@ function solution(str) {
 }
 
 console.log(solution('abdsdfsdf'));
+/*
+since match will only match two of any character, we just add a _ on the end... 
+why? 
+because when we return it, the underscore will be ommited if it's odd because it's only matching pairs
+*/
