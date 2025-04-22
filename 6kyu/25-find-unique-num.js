a52ee8bde3df18439a5f178b31c87fe8063e1fa9
elementtest <elementtest8@gmail.com>
2025-04-22 21:37:29 +0800

    new challenge find unique number in array

---
+++ 6kyu/25-find-unique-num.js
@@ -0,0 +1,14 @@
+/*
+there is an array with some numbers.  All numbers are equal except for one.  Try to find it.   
+it is guaranteed that array contains at least 3 numbers 
+the tests contain some very huge arrays. so think about performance.  
+*/
+
+function findUniq(arr) {
+
+
+}
+
+/*
+
+*/
