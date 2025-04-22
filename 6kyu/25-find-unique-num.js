6e3a48187689f656821451ee37bc4dd16e8efcd7
elementtest <elementtest8@gmail.com>
2025-04-22 22:19:55 +0800

    edit

--- 6kyu/25-find-unique-num.js
+++ 6kyu/25-find-unique-num.js
@@ -5,7 +5,11 @@ the tests contain some very huge arrays. so think about performance.
 */
 
 function findUniq(arr) {
-    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i+1]) {
+            return arr[i+1];
+        }
+    }
     }
 
 console.log(findUniq([1,1,1,2,1,1]));
