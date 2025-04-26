c356de8286364cc5f44b40c656c468ff392533c1
elementtest <elementtest8@gmail.com>
2025-04-26 12:41:31 +0800

    start sort the odd challenge

---
+++ 6kyu/26-sort-the-odd.js
@@ -0,0 +1,10 @@
+/*
+you will be given an array of numbers.  you have to sort the odd
+numbers in ascending order while leaving the even numbers at their original positions.  
+so have to determine if it's even first can use % 2 to determine it.  
+second thing is the odd numbers could use something like sort... 
+but how do we keep the order?  maybe two loops.  can have a check on each of the numbers that sounds like the best way to do it. 
+first loop goes through the array. second loop executes on each element
+in the array.  deletes the even numbers so if number % 2 is zero then 
+dont add but if not then add to new array.  new array.  sort... smallest to biggest.. 
+*/
