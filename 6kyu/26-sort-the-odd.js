1b277f212078026787e6105db8c323499d02b8e7
elementtest <elementtest8@gmail.com>
2025-04-29 22:08:03 +0800

    complete

--- 6kyu/26-sort-the-odd.js
+++ 6kyu/26-sort-the-odd.js
@@ -8,3 +8,35 @@ first loop goes through the array. second loop executes on each element
 in the array.  deletes the even numbers so if number % 2 is zero then 
 dont add but if not then add to new array.  new array.  sort... smallest to biggest.. 
 */
+
+function sortArray(array) {
+    let oddArr = [];
+    for (let a = 0; a < array.length; a++) {
+        if (array[a] % 2 !== 0) {
+            oddArr.push(array[a]);
+        }
+    }
+    oddArr = oddArr.sort((a,b) => a - b);
+
+    let totalArr = array.map(num => {
+        if (num % 2 !== 0) {
+            return oddArr.shift();
+        } else {
+            return num;
+        }
+    })
+    
+    //console.log(oddArr);
+    return totalArr;
+
+}
+
+console.log(sortArray([5,3,2,8,1,4]));
+
+
+/*
+function sortArray(array) {
+const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+return array.map((x) => x % 2 ? odd.shift() : x);
+}
+*/
