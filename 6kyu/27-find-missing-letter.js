ca30b18b483411e8ea2f2bc8e45d579c0eecdfb2
elementtest <elementtest8@gmail.com>
2025-05-02 21:50:18 +0800

    new challenge>> find missing letter

---
+++ 6kyu/27-find-missing-letter.js
@@ -0,0 +1,11 @@
+/*
+input: array of increasing letters from a
+output: missing letter in the array
+i.e. 
+['a', 'b', 'c', 'e']
+output 'd'
+also this is case sensitive... so 
+i.e. 
+['D', 'E', 'F', 'H']
+output 'G'
+*/
