d72fea8b708c51e6591e40104395f5d921d8b9ce
elementtest <elementtest8@gmail.com>
2025-05-05 19:57:48 +0800

    solved

--- 6kyu/27-find-missing-letter.js
+++ 6kyu/27-find-missing-letter.js
@@ -11,7 +11,38 @@ output 'G'
 */
 
 function findMissingLetter(array) {
-    return array.charCodeAt();
+    let missingNo = '';
+    for (let i = 1; i < array.length; i++) {
+        console.log('current one');
+        let firstt = (array[i].charCodeAt(0));
+        console.log('one before it');
+        let secondd = (array[i-1].charCodeAt(0));
+        console.log('ok here is the subtraction');
+
+        if (firstt - secondd !== 1) {
+
+            console.log('found the bad one');
+            missingNo = (firstt -1);
+
+            console.log('the missing number is');
+
+            console.log(missingNo);
+
+            console.log(firstt);
+
+            console.log(array[i]);
+        }
+
+        
+    }
+
+    console.log('final return');
+    return String.fromCharCode(missingNo);
+    
+    console.log('what is the first one');
+    console.log(array[0]);
 }
 
-console.log(findMissingLetter(['a','b','c','d']));
+//console.log(findMissingLetter(['a','b','c','e']));
+
+console.log(findMissingLetter(['E','F','G','I']));
