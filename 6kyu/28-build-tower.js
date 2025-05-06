9e17ffd3d2cd66b9029b1ccffed2b7f6c6c1df64
elementtest <elementtest8@gmail.com>
2025-05-06 22:11:00 +0800

    k

--- 6kyu/28-build-tower.js
+++ 6kyu/28-build-tower.js
@@ -4,3 +4,24 @@ output : a tower made of asterisks ' * ' increasing in size with each floor star
 */
 
 function towerBuilder(nFloors) {
+    let arr = ['*'];
+    let countt = 1;
+    for (let i = 0; i < nFloors; i++) { 
+        countt += 2;
+        
+
+        }
+    return arr;
+
+}
+
+
+console.log(towerBuilder(3));
+
+/*
+what's the relationship from top and with each successive floor number?  
+1st floor: 1... 2nd floor: 3.... 3rd floor: 5 .... 4th floor: 7 ... 5th floor: 9... 
+
+so starts at one and then adds 2 every time 
+
+*/
