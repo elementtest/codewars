b0a5735d57dc809e429f45ca8d730516d2f06185
elementtest <elementtest8@gmail.com>
2025-05-10 12:56:26 +0800

    code successfully working small style issues remain

--- 6kyu/28-build-tower.js
+++ 6kyu/28-build-tower.js
@@ -1,27 +1,36 @@
 /*
-input : given a certain # of floors
-output : a tower made of asterisks ' * ' increasing in size with each floor starting from top
+input: number of floors
+output: asterisk
+(surrounded by equal amount of whitespace on each side.)
+
+['     *     '] // hit space bar five times
+['    ***    '] // hit space bar four times
+['   *****   '] // hit space bar three times
+['  *******  '] // hit space bar twice
+[' ********* '] // hit space bar once
+['***********'] // hit space bar zero times
+
 */
 
 function towerBuilder(nFloors) {
-    let arr = ['*'];
-    let countt = 1;
-    for (let i = 0; i < nFloors; i++) { 
-        countt += 2;
-        
-
-        }
-    return arr;
+    let spacee = 5;
+    let astr = 1;
+    let arr = [];
+    for (let i = 0; i < nFloors; i++) {
+        arr.push(('"' + " ".repeat(spacee)) + ("*".repeat(astr)) + (" ".repeat(spacee)) + '"') 
+        spacee -= 1;
+        astr += 2;
+    }
+    return arr.join(",\n");
 
 }
 
 
-console.log(towerBuilder(3));
+console.log(towerBuilder(6));
 
 /*
 what's the relationship from top and with each successive floor number?  
 1st floor: 1... 2nd floor: 3.... 3rd floor: 5 .... 4th floor: 7 ... 5th floor: 9... 
 
 so starts at one and then adds 2 every time 
-
 */
