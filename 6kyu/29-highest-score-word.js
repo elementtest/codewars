8405d32a5cbbe1bd0e6fec60394baf61edbaeabd
elementtest <elementtest8@gmail.com>
2025-05-14 21:49:17 +0800

    new challenge

---
+++ 6kyu/29-highest-score-word.js
@@ -0,0 +1,20 @@
+/*
+each letter scores points according to position in the alphabet
+i.e.
+a = 1, b = 2, c = 3 etc
+
+for example the score of abad is 8 (1 + 2 + 1 + 4) 
+
+you need to return the highest scoring word as a string
+
+if two words score the same, return the word that appears earliest in the original string
+
+all letters will be lowercase and all inputs will be valid
+
+
+TLDR: 
+
+input : string of letters
+output : score based on 1 = a and 26 = z
+*/
+
