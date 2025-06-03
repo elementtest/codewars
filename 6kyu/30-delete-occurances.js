0de3671e790b1faa2c3a0d0486ab8425b629df43
elementtest <elementtest8@gmail.com>
2025-06-03 21:45:50 +0800

    new : repeat numbers challenge

---
+++ 6kyu/30-delete-occurances.js
@@ -0,0 +1,16 @@
+/*
+input: numbers
+
+output:  given N only allow N number of repeats.  do not change the order of the numbers
+
+i.e.
+N is 2
+[1,2,3,1,2,1,2,3]
+becomes: 
+[1,2,3,1,2,3]
+
+N is 1
+[20,37,20,21]
+becomes: 
+[20,37,21]
+*/
