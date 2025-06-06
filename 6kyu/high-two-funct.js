591b70c454d5c548a50577ab1d2e89d08a90c453
elementtest <elementtest8@gmail.com>
2025-06-06 22:51:12 +0800

    finish higher order function practice

--- 6kyu/high-two-funct.js
+++ 6kyu/high-two-funct.js
@@ -1,33 +1,27 @@
-const radius = [3,1,2,4];
+//const radius = [3,1,2,4];
 
-const basicFunct = function (radius, loopVar, numm) {
+const basicFunct = function (firstt, radius, loopVar, numm) {
     const output = [];
     for (let i = 0; i < radius.length; i++) {
-        output.push(loopVar * Math.pow(radius[i], numm))
+        output.push(firstt * loopVar * Math.pow(radius[i], numm))
     }
     return output;
 }
 
 const calculateArea = function (callback) {
-    return callback([3,1,2,4], (Math.PI), 2)
+    return callback(1, [3,1,2,4], (Math.PI), 2)
 };
 
-console.log(calculateArea(basicFunct));
+//console.log(calculateArea(basicFunct));
 
 const calculateCircumference = function (callback) {
-    callback([3,1,2,4], (Math.))
-    const output = [];
-    for(let i = 0; i<radius.length; i++) {
-        output.push(2* Math.PI * radius[i]);
-    }
-    return output;
+    return callback(2,[3,1,2,4], (Math.PI), 1)
 };
+console.log(calculateCircumference(basicFunct));
 
-const calcDiameter = function (radius) {
-    const output = [];
-    for(let i = 0; i<radius.length; i++) {
-        output.push(2 * radius[i]);
-    }
-    return output;
+const calcDiameter = function (callback) {
+    return callback(2,[3,1,2,4], 1,1)
 }
+//console.log(calcDiameter(basicFunct));
+
 
