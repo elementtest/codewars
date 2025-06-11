bb26633012d0b1679b1862b4911341f2be056670
elementtest <elementtest8@gmail.com>
2025-06-11 19:56:21 +0800

    updat

--- 6kyu/close-two.js
+++ 6kyu/close-two.js
@@ -1,3 +1,19 @@
 /*
 continued practice of closures
 */
+
+function outerFunction() {
+    let outerVariable = 'I am outside!';
+
+    function innerFunction() {
+        console.log(outerVariable);
+        let innerVariable = 'I am inside!';
+        console.log(innerVariable);
+    }
+
+    innerFunction();
+}
+
+outerFunction();
+
+
