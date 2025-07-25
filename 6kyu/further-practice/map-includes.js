ab380a9b2476996fa3121e10146f3c8fa0b55587
elementtest <elementtest8@gmail.com>
2025-07-25 22:19:57 +0800

    kmg

--- 6kyu/further-practice/map-includes.js
+++
@@ -1,9 +0,0 @@
-const sentence = ['go', 'please', 'fudge', 'yourself', 'shizz', 'great', 'boom'];
-
-const badWords = ['fudge', 'shizz', 'boom'];
-
-//check if the sentence has fudge in it and if so replace with "BADWORD"
-
-const cleanSent = sentence.map(word => badWords.includes(word) ? 'BADWORD' : word).join(' ');
-
-console.log(cleanSent);
