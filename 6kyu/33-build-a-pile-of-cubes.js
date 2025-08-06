90816f90a0bdf8ddd7fb1ee3f220df1cf244db74
elementtest <elementtest8@gmail.com>
2025-08-06 20:10:35 +0800

    k

---
+++ 6kyu/33-build-a-pile-of-cubes.js
@@ -0,0 +1,6 @@
+/*
+task.. given a total volume of m... find the number of n to fill the volume... 
+n is in a pyramid structure.  i.e. top is 1 * 1 * 1 floor under is 2 * 2 * 2.... 
+third floor down is 3 * 3 * 3 ... etc all the way until you fill m
+
+*/
