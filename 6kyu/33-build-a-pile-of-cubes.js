3f448665481928bbf9c35b0761e8a418a353ca2d
elementtest <elementtest8@gmail.com>
2025-08-07 21:54:27 +0800

    k

--- 6kyu/33-build-a-pile-of-cubes.js
+++ 6kyu/33-build-a-pile-of-cubes.js
@@ -2,5 +2,19 @@
 task.. given a total volume of m... find the number of n to fill the volume... 
 n is in a pyramid structure.  i.e. top is 1 * 1 * 1 floor under is 2 * 2 * 2.... 
 third floor down is 3 * 3 * 3 ... etc all the way until you fill m
-
 */
+
+function findNb(m) {
+    let check = 0;
+    for (let n = 1; check < m; n++) {
+        check += (Math.pow(n,3));
+        console.log(check);
+        if (check === m) {
+            return n;
+        }
+    }
+    if (check !== m) {return -1};
+
+}
+
+console.log(findNb());
