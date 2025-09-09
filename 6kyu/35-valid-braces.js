fed0e47c403d20ec1d041924450acc8569921f85
elementtest <elementtest8@gmail.com>
2025-09-09 21:56:16 +0800

    k

--- 6kyu/35-valid-braces.js
+++ 6kyu/35-valid-braces.js
@@ -5,3 +5,18 @@ return true if the string is valid and false if it is invalid...
 all input strings will be nonempty, and will only consist of parentheses, brackets and curly braces
 ()[]{}
 */
+
+
+function validParentheses(parenStr) {
+    for (let i = 0; i < parenStr.length; i++) {
+
+    }
+
+}
+
+/*
+search through the string.... basically has to match the character right?  so... 
+have a for loop... loop thru the string... look for the other character....
+do two for loops.
+
+*/
