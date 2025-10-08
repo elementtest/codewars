0ee0e0f90d8b302fe51e7cecd588d46794ccbc98
elementtest <elementtest8@gmail.com>
2025-10-08 19:04:19 +0800

    k

---
+++ 6kyu/35-practice-exercise.js
@@ -0,0 +1,15 @@
+/*
+problem: 
+write a function that takes a string of HTML-like tags (no attributs just opening and closing tags) 
+and returns true if the tags are properly nested and matched and false if otherwise...
+
+isValidHTML("<div><p></p></div>")         // true
+isValidHTML("<div><p></div></p>")         // false
+isValidHTML("<div><p><span></span></p></div>") // true
+isValidHTML("<div><p></span></p></div>")  // false
+
+*/
+
+function htmlChecker(html) {
+
+}
